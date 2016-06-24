a781b77ce2a2a12d1006b207410ff1173b6062da
connerjohnston631 <connerjohnston631@gmail.com>
2016-06-24 10:36:23 -0700

    code reduced

--- src/elements.js
+++ src/elements.js
@@ -1,189 +1,222 @@
 /* verifies and packs the info into an element list, functional parallel to object instantiation */
-function new_element(label, attributes, content, elements){
+const label = 'label';
+const classes = 'classes';
+const attributes = 'attributes';
+const content = 'content';
+const children = 'children';
+
+function emptyElement(){
 	/* add checks for validation here */
-	return [label, attributes, content, elements];
+	return {label: '', classes: [], attributes: {}, content: '', children: []};
+}
+
+function lbl(element, str){
+	return {label: str, classes: element[classes], attributes: element[attributes], content: element[content], children: element[children]};
+}
+
+function clss(element, clsss){
+	return {label: element[label], classes: clsss, attributes: element[attributes], content: element[content], children: element[children]};
+}
+
+function cntnt(element, cont){
+	return {label: element[label], classes: element[classes], attributes: element[attributes], content: cont, children: element[children]};
+}
+
+function attrbts(element, attrs){
+	return {label: element[label], classes: element[classes], attributes: attrs, content: element[content], children: element[children]};
+}
+
+function chldrn(element, child_elements){
+	return {label: element[label], classes: element[classes], attributes: element[attributes], content: element[content], children: child_elements};
 }
 
 function add_attributes(element, attributes){
-	var new_attributes = element[1];
+	var new_attributes = element[attributes];
 	for(var attr in attributes){
 		new_attributes[attr] = attributes[attr];
 	}
-	return [element[0], new_attributes, element[2], element[3]];
+	return attrbts(element, new_attributes);
+}
+
+function add_classes(element, clsss){
+	return clss(element, element[classes].concat(clsss));
 }
 
 function add_children(element, elements){
-	return [element[0], element[1], element[2], element[3].concat(elements)]; 
+	return chldrn(element, element[children].concat(elements)); 
 }
-
 
 /* these are the functions that generate the various html types. add as needed */
-function h1(content){
-	return new_element('h1', {}, content, []);
-}
+// function h1(content){
+// 	return new_element('h1', {}, content, []);
+// }
 
-function h2(content){
-	return new_element('h2', {}, content, []);
-}
+// function h2(content){
+// 	return new_element('h2', {}, content, []);
+// }
 
-function h3(content){
-	return new_element('h3', {}, content, []);
-}
+// function h3(content){
+// 	return new_element('h3', {}, content, []);
+// }
 
-function h4(content){
-	return new_element('h4', {}, content, []);
-}
+// function h4(content){
+// 	return new_element('h4', {}, content, []);
+// }
 
-function h5(content){
-	return new_element('h5', {}, content, []);
-}
+// function h5(content){
+// 	return new_element('h5', {}, content, []);
+// }
 
-function h6(content){
-	return new_element('h6', {}, content, []);
-}
+// function h6(content){
+// 	return new_element('h6', {}, content, []);
+// }
 
-function p(content){
-	return new_element('p', {}, content, []);
-}
+// function p(content){
+// 	return new_element('p', {}, content, []);
+// }
 
-function small(content){
-	return new_element('small', {}, content, []);
-}
+// function small(content){
+// 	return new_element('small', {}, content, []);
+// }
 
-function mark(content){
-	return new_element('mark', {}, content, []);
-}
+// function mark(content){
+// 	return new_element('mark', {}, content, []);
+// }
 
-function abbr(content){
-	return new_element('abbr', {}, content, []);
-}
+// function abbr(content){
+// 	return new_element('abbr', {}, content, []);
+// }
 
-function blockquote(content){
-	return new_element('blockquote', {}, content, []);
-}
+// function blockquote(content){
+// 	return new_element('blockquote', {}, content, []);
+// }
 
-function dl(content){
-	return new_element('dl', {}, content, []);
-}
+// function dl(content){
+// 	return new_element('dl', {}, content, []);
+// }
 
-function code(content){
-	return new_element('code', {}, content, []);
-}
+// function code(content){
+// 	return new_element('code', {}, content, []);
+// }
 
-function kbd(content){
-	return new_element('kbd', {}, content, []);
-}
+// function kbd(content){
+// 	return new_element('kbd', {}, content, []);
+// }
 
-function pre(content){
-	return new_element('pre', {}, content, []);
-}
+// function pre(content){
+// 	return new_element('pre', {}, content, []);
+// }
 
-function lnk(url, text){
-	return new_element('a', {'href': url}, text, []);
-}
+// function lnk(url, text){
+// 	return new_element('a', {'href': url}, text, []);
+// }
 
-function div(){
-	return new_element('div', {}, EMPTY_STRING, []);
-}
+// function div(){
+// 	return new_element('div', {}, EMPTY_STRING, []);
+// }
 
-function span(){
-	return new_element('span', {}, EMPTY_STRING, []);
-}
+// function span(){
+// 	return new_element('span', {}, EMPTY_STRING, []);
+// }
 
-function tr(){
-	return new_element('tr', {}, EMPTY_STRING, []);
-}
+// function tr(){
+// 	return new_element('tr', {}, EMPTY_STRING, []);
+// }
 
-function td(){
-	return new_element('td', {}, EMPTY_STRING, []);
-}
+// function td(){
+// 	return new_element('td', {}, EMPTY_STRING, []);
+// }
 
-function table(){
-	return new_element('table', {}, EMPTY_STRING, []);
-}
+// function table(){
+// 	return new_element('table', {}, EMPTY_STRING, []);
+// }
 
-/* layout functions for html5 */
-function header(){
-	return new_element('header', {}, EMPTY_STRING, []);
-}
+// /* layout functions for html5 */
+// function header(){
+// 	return new_element('header', {}, EMPTY_STRING, []);
+// }
 
-function nav(){
-	return new_element('nav', {}, EMPTY_STRING, []);
-}
+// function nav(){
+// 	return new_element('nav', {}, EMPTY_STRING, []);
+// }
 
-function section(){
-	return new_element('section', {}, EMPTY_STRING, []);
-}
+// function section(){
+// 	return new_element('section', {}, EMPTY_STRING, []);
+// }
 
-function article(){
-	return new_element('article', {}, EMPTY_STRING, []);
-}
+// function article(){
+// 	return new_element('article', {}, EMPTY_STRING, []);
+// }
 
-function aside(){
-	return new_element('aside', {}, EMPTY_STRING, []);
-}
+// function aside(){
+// 	return new_element('aside', {}, EMPTY_STRING, []);
+// }
 
-function footer(){
-	return new_element('footer', {}, EMPTY_STRING, []);
-}
+// function footer(){
+// 	return new_element('footer', {}, EMPTY_STRING, []);
+// }
 
-function details(){
-	return new_element('details', {}, EMPTY_STRING, []);
-}
+// function details(){
+// 	return new_element('details', {}, EMPTY_STRING, []);
+// }
 
-function summary(){
-	return new_element('summary', {}, EMPTY_STRING, []);
-}
+// function summary(){
+// 	return new_element('summary', {}, EMPTY_STRING, []);
+// }
 
-/* end new html5 layout elements */
+// /* end new html5 layout elements */
 
-/* bootstrap elements, by classname */
-function jumbotron(){
-	return add_attributes(div(), {'class': 'jumbotron'});
-}
+// /* bootstrap elements, by classname */
+// function jumbotron(){
+// 	return add_attributes(div(), {'class': 'jumbotron'});
+// }
 
-function pageHeader(){
-	return add_attributes(div(), {'class': 'page-header'});
-}
+// function pageHeader(){
+// 	return add_attributes(div(), {'class': 'page-header'});
+// }
 
-function pageFooter(){
-	return add_attributes(div(), {'class': 'page-footer'});
-}
+// function pageFooter(){
+// 	return add_attributes(div(), {'class': 'page-footer'});
+// }
 
-function grid(width, size){
-	return add_attributes(div(), {'class': 'col-' + size + '-' + width.toString()});
-}
+// function container(){
+// 	return add_attributes(div(), {'class': 'container'});
+// }
 
-function smGrid(width){
-	return grid(width, 'sm');
-}
+// function grid(width, size){
+// 	return add_attributes(div(), {'class': 'col-' + size + '-' + width.toString()});
+// }
 
-function xsGrid(width){
-	return grid(width, 'xs');
-}
+// function smGrid(width){
+// 	return grid(width, 'sm');
+// }
 
-function mdGrid(width){
-	return grid(width, 'md');
-}
+// function xsGrid(width){
+// 	return grid(width, 'xs');
+// }
 
-function lgGrid(width){
-	return grid(width, 'lg');
-}
+// function mdGrid(width){
+// 	return grid(width, 'md');
+// }
 
-function row(){
-	return add_attributes(div(), {'class': 'row'});
-}
+// function lgGrid(width){
+// 	return grid(width, 'lg');
+// }
 
-function blockquoteReverse(content){
-	return add_attributes(blockquote(content), {'class': 'blockquote-reverse'});
-}
+// function row(){
+// 	return add_attributes(div(), {'class': 'row'});
+// }
 
-function textCenter(element){
-	return add_attributes(element, {'class': 'text-center'});
-}
+// function blockquoteReverse(content){
+// 	return add_attributes(blockquote(content), {'class': 'blockquote-reverse'});
+// }
 
-function glyphcon(glyph){
-	return add_attributes(span(), {'class': 'glyphcons glyphcons-' + glyph});
-}
+// function textCenter(element){
+// 	return add_attributes(element, {'class': 'text-center'});
+// }
+
+// function glyphcon(glyph){
+// 	return add_attributes(span(), {'class': 'glyphcons glyphcons-' + glyph});
+// }
 
 
--- src/main.js
+++ src/main.js
@@ -1,49 +1,49 @@
 /* This code is used to generate html code from functional javascript */
-const LEFT_BRACKET = "<";
-const RIGHT_BRACKET = ">";
-const SLASH = "/";
-const NL = "\n";
-const HEADER_LABEL = "head";
-const BODY_LABEL = "body";
-const DOCTYPE_STR = '<!DOCTYPE html>';
+const LEFT_BRACKET = '<';
+const RIGHT_BRACKET = '>';
+const SLASH = '/';
+const NL = '\n';
 const SPACE = ' ';
 const EMPTY_STRING = '';
 const QUOTE = '\"';
 const EQUALS = '=';
 const TAB = '\t';
+const CLASS = 'class';
+
 
 /* html objects (things surrounded by tags, like a div) will have: 
- * [label, attributes, content, list of inner objects]. This quadruple will
+ * {label, classes, attributes, content, list of inner objects}. This will
  * be referred to as an element. also need to include content in between tags... 
  */
 function bracketize(tag){
 	return LEFT_BRACKET + tag + RIGHT_BRACKET;
 }
 
-function attributeStr(attributes){
+function classStr(classes){
+	return classes.length == 0 ? EMPTY_STRING : [SPACE, CLASS, EQUALS, QUOTE, classes.join(SPACE), QUOTE].join(EMPTY_STRING);
+}
+
+function attributeStr(element){
 	result = EMPTY_STRING;
-	for(var attr in attributes){result = result.concat([SPACE, attr, EQUALS, QUOTE, attributes[attr], QUOTE].join(EMPTY_STRING));}
-	return result;
+	for(var attr in element[attributes]){result = result.concat([SPACE, attr, EQUALS, QUOTE, element[attributes][attr], QUOTE].join(EMPTY_STRING));}
+	return classStr(element[classes]) + result;
 }
 
 function openTag(element){
 	/* TODO: include attributes and content*/
-	return bracketize(element[0] + attributeStr(element[1])) + element[2];
+	return bracketize(element[label] + attributeStr(element)) + element[content];
 }
 
 function closeTag(element){
-	return bracketize(SLASH + element[0]);
+	return bracketize(SLASH + element[label]);
 }
 
 /* this is the most intense of the functions */
 /* do not touch unless you like recursion */
 /* possibly include a check for depth */
-function elementStr(element, tabs){
-	
-	function helper(element){
-		return elementStr(element, tabs + TAB);
-	}
-	return [tabs, openTag(element), NL, element[3].map(helper).join(EMPTY_STRING), tabs, closeTag(element), NL].join(EMPTY_STRING);
+function elementStr(element, tabs){	
+	function helper(element){ return elementStr(element, tabs + TAB); }
+	return [tabs, openTag(element), NL, element[children].map(helper).join(EMPTY_STRING), tabs, closeTag(element), NL].join(EMPTY_STRING);
 }
 
 /* use this as generation function for a single top element */
