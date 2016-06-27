382abb7feb5c3c3fdc214c835e3250a7a9a45fd8
connerjohnston631 <connerjohnston631@gmail.com>
2016-06-26 19:10:48 -0700

    refactor and changes

--- src/js/elements.js
+++ src/js/elements.js
@@ -10,38 +10,38 @@ function emptyElement(){
 	return {label: EMPTY_STRING, classes: [], attributes: {}, content: EMPTY_STRING, children: []};
 }
 
-function lbl(element, str){
+function withLabel(element, str){
 	return {label: str, classes: element[classes], attributes: element[attributes], content: element[content], children: element[children]};
 }
 
-function clss(element, clsss){
+function withClass(element, clsss){
 	return {label: element[label], classes: clsss, attributes: element[attributes], content: element[content], children: element[children]};
 }
 
-function cntnt(element, cont){
+function withContent(element, cont){
 	return {label: element[label], classes: element[classes], attributes: element[attributes], content: cont, children: element[children]};
 }
 
-function attrbts(element, attrs){
+function withAttributes(element, attrs){
 	return {label: element[label], classes: element[classes], attributes: attrs, content: element[content], children: element[children]};
 }
 
-function chldrn(element, child_elements){
+function withChildren(element, child_elements){
 	return {label: element[label], classes: element[classes], attributes: element[attributes], content: element[content], children: child_elements};
 }
 
-function add_attributes(element, attributes){
+function withMoreAttributes(element, attributes){
 	var new_attributes = element[attributes];
 	for(var attr in attributes){
 		new_attributes[attr] = attributes[attr];
 	}
-	return attrbts(element, new_attributes);
+	return withAttributes(element, new_attributes);
 }
 
-function add_classes(element, clsss){
-	return clss(element, element[classes].concat(clsss));
+function withMoreClasses(element, clsss){
+	return withClass(element, element[classes].concat(clsss));
 }
 
-function add_children(element, elements){
-	return chldrn(element, element[children].concat(elements)); 
+function withMoreChildren(element, elements){
+	return withChildren(element, element[children].concat(elements)); 
 }
